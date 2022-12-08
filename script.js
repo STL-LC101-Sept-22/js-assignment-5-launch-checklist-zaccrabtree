edc1c8f7e780def9b562477f4eeb02276c8c2859
Zac Crabtree <zac.crabtree1@gmail.com>
2022-12-08 12:33:49 -0600

    initial commit

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 // Write your JavaScript code here!
 
 window.addEventListener("load", function() {
+      //need to add alert if any of the form fields are empty
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function(event) {
+        console.log("Submitted")
+        let pilotNameInput = document.querySelector("input[name=pilotName]");
+        let copilotNameInput = document.querySelector("input[name=copilotName]");
+        let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+        let cargoMassInput = document.querySelector("input[name=cargoMass]");
+        if (pilotNameInput.value === "" || copilotNameInput.value === "" || fuelLevelInput.value === "" || cargoMassInput.value === "") {
+            alert("All fields are required!");
+        } else if (isNaN(pilotNameInput.value) === true || isNaN(copilotNameInput.value) === true || isNaN(fuelLevelInput.value) === false || isNaN(cargoMassInput.value) === false) {
+            alert("Make sure to enter valid information for each field!");
+        }   
+        event.preventDefault()
 
-   let listedPlanets;
+    })
+
+   let listedPlanets = [];
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
    let listedPlanetsResponse;
    listedPlanetsResponse.then(function (result) {
